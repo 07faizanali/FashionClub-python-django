2e204cb658aae014cb46bd88d46feb157c172994
Faizan Ali <mohd.faizanali.m.z@gmail.com>
2023-03-17 15:01:01 +0530

    Third commit

--- Fashion/settings.py
+++ Fashion/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
@@ -131,3 +133,9 @@ STATICFILES_DIRS =[
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS ={
+    messages.ERROR: 'DANGER',
+}
